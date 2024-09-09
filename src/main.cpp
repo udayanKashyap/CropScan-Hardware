fb82fb71477b268c2a7c89a4322ca58aa5c20d3e
udayan <kashyapudayan@gmail.com>
2024-09-09 15:15:45 +0530

    Voltage is updated in sets of N voltages on input. Branch v2. updating voltages in a string array

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 
+// #define FIREBASEJSON_USE_FS
+
 #include "wifi.h"
 #include "firebase.h"
 #include "led.h"
 #include "ldr.h"
+
+#define BUTTON1 21
 
 // Timer variables
 unsigned long sendDataPrevMillis = 0;
 unsigned long timerDelay = 1000;
 LED led1;
 LDR ldr1;
+void sendBurst();
 
 void setup()
 {
@@ -26,20 +31,43 @@ void loop()
 {
     int intensity = 0;
 
-    float voltage;
     // Database requests
     if (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)
     {
         sendDataPrevMillis = millis();
-        voltage = ldr1.readLDR() / 1000.00;
-
         if (Firebase.ready())
         {
             intensity = getIntensity("/intensity");
-            setLDR(voltage);
         }
-
         led1.setLED(intensity);
-        Serial.printf("Timestamp: %d Intensity: %d    Voltage: %f\n", sendDataPrevMillis / 1000, intensity, voltage);
+        Serial.printf("Timestamp: %d Intensity: %d\n", sendDataPrevMillis / 1000, intensity);
+    }
+    if (digitalRead(BUTTON1) == LOW)
+    {
+        Serial.println("Sending voltage data...");
+        sendDataPrevMillis = 0;
+        sendBurst();
+        Serial.println("Voltage Data Sent");
+    }
+}
+
+void sendBurst()
+{
+    int N = 20;
+    float voltage[N];
+    int counter = 0;
+    while (counter < N)
+    {
+        if (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)
+        {
+            sendDataPrevMillis = millis();
+            voltage[counter] = ldr1.readLDR() / 1000.00;
+            Serial.printf("Timestamp: %d    Voltage: %f\n", sendDataPrevMillis / 1000, voltage[counter]);
+            counter++;
+        }
+    }
+    if (Firebase.ready())
+    {
+        setLdrArray(voltage, N);
     }
 }
